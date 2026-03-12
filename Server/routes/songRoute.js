dbc9c5019f2ab662166037efc19957a62f186618
sabinsubediofficial <sabinsubediofficial@gmail.com>
2026-03-12 08:46:00 +0545

    API for search by Id and Strings

--- Server/routes/songRoute.js
+++ Server/routes/songRoute.js
@@ -28,4 +28,40 @@ router.post('/', async (req, res) => {
   }
 });
 
+//Get songs by id
+router.get('/:id', async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.id);
+
+    if (!song) {
+      return res.status(400).json({ message: 'Song not found' });
+    }
+    res.json(song);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//Get songs by strings
+router.get('/search/:query', async (req, res) => {
+  try {
+    const { query } = req.params;
+    const searchRegex = new RegExp(query, 'i');
+
+    const song = await Song.find({
+      $or: [
+        { title: { $regex: searchRegex } },
+        { artist: { $regex: searchRegex } },
+      ],
+    });
+
+    if (song.length === 0) {
+      return res.status(404).json({ message: 'No matches found' });
+    }
+    res.json(song);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
